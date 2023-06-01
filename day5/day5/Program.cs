5c7465556eb6908b63e3b2d27093ae41db89bcc2
student11 <133848692+1Chuppi1@users.noreply.github.com>
2023-06-01 10:39:42 +0300

    if xy

---
+++ day5/day5/Program.cs
@@ -0,0 +1,18 @@
+﻿using day5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeesBook book = new EmployeesBook();
+            book.Employeesbook();
+        }
+    }
+}
